a9e19f779830aac4655449b39f627e7070b81e97
Mojca Hercog <mojca.hercog@gmail.com>
2020-03-05 14:05:37 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-flask import Flask, render_template, request, make_response, redirect, url_for
+from flask import Flask, render_template, request, make_response, redirect, url_for
 from models import User, db
 
 app = Flask(__name__)
 db.create_all()  # create (new) tables in the database
 
 @app.route ("/", methods=["GET"])
-def
+def index():
+    return render_template("index.html")
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
